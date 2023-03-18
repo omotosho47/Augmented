2a15b18f731db9bb639218e15f8aa99da2361e35
omotosho47 <kmm22njy@bangor.ac.uk>
2023-03-18 09:30:25 +0100

    Feat: Added Ar js to scene

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -78,13 +78,13 @@ function Chart() {
   const heightScale = d3.scaleLinear().domain([0, 100]).range([0, 8]);
   const tickValues = heightScale.ticks(10);
 
-    const zLabels = [
-      { position:  "-8 1 -0.9", text: "Humidity" },
-      { position: "-8, 1 0", text: "Wind Gauge" },
-      { position: "-8, 1 2.5", text: "Wind Speed" },
-      { position: "-8, 1 4.5", text: "Temperature" },
-      { position: "-8, 1 -1.8", text: "Cloud Cover" },
-    ];
+  const zLabels = [
+    { position: "-8 1 -0.9", text: "Humidity" },
+    { position: "-8, 1 0", text: "Wind Gauge" },
+    { position: "-8, 1 2.5", text: "Wind Speed" },
+    { position: "-8, 1 4.5", text: "Temperature" },
+    { position: "-8, 1 -1.8", text: "Cloud Cover" },
+  ];
 
   // Manipulating the DOM
 
@@ -176,185 +176,184 @@ function Chart() {
   return (
     <>
       <ToastContainer />
-      <a-scene cursor="rayOrigin: mouse" stats ref={sceneRef} >
-      {/* embedded arjs */}
-        {/* <a-marker-camera preset='hiro'></a-marker-camera>     */}
-        <a-entity camera="" position="0 10 19" look-controls=""></a-entity>
-        <Legend />
-        <Text
-          id="center"
-          value="Climate data for Bangor University"
-          position="0 13.5 2"
-          scale="6 6 6"
-        />
-        <Text
-          id="center"
-          value=" Time Period"
-          position="0 -0.3 6.5"
-          scale="5 5 5"
-        />
-        {tickValues.map((item, index) => (
-          <a-entity key={index}>
-            <a-entity
-              line={`start: -7.2 ${heightScale(
-                item
-              )} -6; end: 7.4 ${heightScale(item)} -6; color: lightblue`}
-            ></a-entity>
-            <a-entity
-              line={`start: 7.4 ${heightScale(item)} -6; end: 7.4 ${heightScale(
-                item
-              )} 5; color: lightblue`}
-            ></a-entity>
-            <Text
-              value={item}
-              position={`7.8 ${heightScale(item)} 5`}
-              color="blue"
-            />
-            <Text
-              value={item}
-              position={`-7.6 ${heightScale(item)} -6`}
-              color="blue"
-            />
-          </a-entity>
-        ))}
-        {data.map((item, i) => {
-          const date = item.datetimeStr.slice(0, 10)
-          return (
-            <Text
-              key={i}
-              value={date}
-              position={`${i * 1.35 - 4} 1 6.5`}
-              color="blue"
-              rotation="-60 -60 -10"
-            />
-          );
-        })}
-       <a-entity>
-       {zLabels.map((item, i) => {
-          return(
-            <Text
-            key={i}
-            value={item.text}
-            position={item.position}
-            color="blue"
-            align="left"
+      <a-scene embedded arjs="sourceType: webcam">
+        <a-marker preset="hiro">
+          <a-entity camera="" position="0 10 19" look-controls=""></a-entity>
+          <Legend />
+          <Text
+            id="center"
+            value="Climate data for Bangor University"
+            position="0 13.5 2"
+            scale="6 6 6"
           />
-          )
-        })}
-       </a-entity>
-        <a-entity ref={humidityRef}>
-          {data.map((item, index) => {
+          <Text
+            id="center"
+            value=" Time Period"
+            position="0 -0.3 6.5"
+            scale="5 5 5"
+          />
+          {tickValues.map((item, index) => (
+            <a-entity key={index}>
+              <a-entity
+                line={`start: -7.2 ${heightScale(
+                  item
+                )} -6; end: 7.4 ${heightScale(item)} -6; color: lightblue`}
+              ></a-entity>
+              <a-entity
+                line={`start: 7.4 ${heightScale(
+                  item
+                )} -6; end: 7.4 ${heightScale(item)} 5; color: lightblue`}
+              ></a-entity>
+              <Text
+                value={item}
+                position={`7.8 ${heightScale(item)} 5`}
+                color="blue"
+              />
+              <Text
+                value={item}
+                position={`-7.6 ${heightScale(item)} -6`}
+                color="blue"
+              />
+            </a-entity>
+          ))}
+          {data.map((item, i) => {
+            const date = item.datetimeStr.slice(0, 10);
             return (
-              <a-cylinder
-                key={index}
-                onClick={() => {
-                  toast.success(
-                    <>
-                      <h2>On {item.datetimeStr.slice(0, 10)}</h2>
-                      <h3>Humidity: {item.humidity}%</h3>
-                      <h3>Weather Condition: {item.conditions}</h3>
-                      <h3>Weather Type: {item.weathertype}</h3>
-                      <h3>Sea Level Pressure: {item.sealevelpressure} hPa</h3>
-                    </>
-                  );
-                }}
-              ></a-cylinder>
+              <Text
+                key={i}
+                value={date}
+                position={`${i * 1.35 - 4} 1 6.5`}
+                color="blue"
+                rotation="-60 -60 -10"
+              />
             );
           })}
-        </a-entity>
-        <a-entity ref={temperatureRef}>
-          {data.map((item, index) => {
-            return (
-              <a-cylinder
-                key={index}
-                onClick={() => {
-                  toast.success(
-                    <>
-                      <h2>On {item.datetimeStr.slice(0, 10)}</h2>
-                      <h3>Temperature: {item.temp}°C</h3>
-                      <h3>Weather Condition: {item.conditions}</h3>
-                      <h3>Weather Type: {item.weathertype}</h3>
-                      <h3>Solar Radiation: {item.solarradiation}W/m²</h3>
-                    </>
-                  );
-                }}
-              ></a-cylinder>
-            );
-          })}
-        </a-entity>
-        <a-entity ref={cloudRef}>
-          {data.map((item, index) => {
-            return (
-              <a-cylinder
-                key={index}
-                onClick={() => {
-                  toast.success(
-                    <>
-                      <h2>On {item.datetimeStr.slice(0, 10)}</h2>
-                      <h3>Cloud Cover: {item.cloudcover}%</h3>
-                      <h3>Weather Condition: {item.conditions}</h3>
-                      <h3>Weather Type: {item.weathertype}</h3>
-                    </>
-                  );
-                }}
-              ></a-cylinder>
-            );
-          })}
-        </a-entity>
-        <a-entity ref={wgustRef}>
-          {data.map((item, index) => {
-            return (
-              <a-cylinder
-                key={index}
-                onClick={() => {
-                  toast.success(
-                    <>
-                      <h2>On {item.datetimeStr.slice(0, 10)}</h2>
-                      <h3>Wind Gauge: {item.wgust}m/s</h3>
-                      <h3>Weather Condition: {item.conditions}</h3>
-                      <h3>Weather Type: {item.weathertype}</h3>
-                      <h3>Wind direction: {item.wdir} degrees</h3>
-                    </>
-                  );
-                }}
-              ></a-cylinder>
-            );
-          })}
-        </a-entity>
-        <a-entity ref={windRef}>
-          {data.map((item, index) => {
-            return (
-              <a-cylinder
-                key={index}
-                onClick={() => {
-                  toast.success(
-                    <>
-                      <h2>On {item.datetimeStr.slice(0, 10)}</h2>
-                      <h3>Wind Speed: {item.wspd}m/s</h3>
-                      <h3>Weather Condition: {item.conditions}</h3>
-                      <h3>Weather Type: {item.weathertype}</h3>
-                      <h3>Sea Level Pressure: {item.sealevelpressure} hPa</h3>
-                    </>
-                  );
-                }}
-              ></a-cylinder>
-            );
-          })}
-        </a-entity>
-        <a-plane
-          color="gray"
-          width={width}
-          height={height}
-          rotation="-90 0 0"
-          position="0.5 0 0.8"
-        ></a-plane>
-        <a-entity
-          line="start: -7.2 -0.7 -6; end: -7.2 10 -6"
-          line__2="start: 7.4 -0.7 -6; end: 7.4 10 -6"
-          line__3="start: 7.4 -0.7 5; end: 7.4 10 5"
-        ></a-entity>
-
-        <a-entity id="center-target" position="0 1.6 -0.5"></a-entity>
+          <a-entity>
+            {zLabels.map((item, i) => {
+              return (
+                <Text
+                  key={i}
+                  value={item.text}
+                  position={item.position}
+                  color="blue"
+                  align="left"
+                />
+              );
+            })}
+          </a-entity>
+          <a-entity ref={humidityRef}>
+            {data.map((item, index) => {
+              return (
+                <a-cylinder
+                  key={index}
+                  onClick={() => {
+                    toast.success(
+                      <>
+                        <h2>On {item.datetimeStr.slice(0, 10)}</h2>
+                        <h3>Humidity: {item.humidity}%</h3>
+                        <h3>Weather Condition: {item.conditions}</h3>
+                        <h3>Weather Type: {item.weathertype}</h3>
+                        <h3>Sea Level Pressure: {item.sealevelpressure} hPa</h3>
+                      </>
+                    );
+                  }}
+                ></a-cylinder>
+              );
+            })}
+          </a-entity>
+          <a-entity ref={temperatureRef}>
+            {data.map((item, index) => {
+              return (
+                <a-cylinder
+                  key={index}
+                  onClick={() => {
+                    toast.success(
+                      <>
+                        <h2>On {item.datetimeStr.slice(0, 10)}</h2>
+                        <h3>Temperature: {item.temp}°C</h3>
+                        <h3>Weather Condition: {item.conditions}</h3>
+                        <h3>Weather Type: {item.weathertype}</h3>
+                        <h3>Solar Radiation: {item.solarradiation}W/m²</h3>
+                      </>
+                    );
+                  }}
+                ></a-cylinder>
+              );
+            })}
+          </a-entity>
+          <a-entity ref={cloudRef}>
+            {data.map((item, index) => {
+              return (
+                <a-cylinder
+                  key={index}
+                  onClick={() => {
+                    toast.success(
+                      <>
+                        <h2>On {item.datetimeStr.slice(0, 10)}</h2>
+                        <h3>Cloud Cover: {item.cloudcover}%</h3>
+                        <h3>Weather Condition: {item.conditions}</h3>
+                        <h3>Weather Type: {item.weathertype}</h3>
+                      </>
+                    );
+                  }}
+                ></a-cylinder>
+              );
+            })}
+          </a-entity>
+          <a-entity ref={wgustRef}>
+            {data.map((item, index) => {
+              return (
+                <a-cylinder
+                  key={index}
+                  onClick={() => {
+                    toast.success(
+                      <>
+                        <h2>On {item.datetimeStr.slice(0, 10)}</h2>
+                        <h3>Wind Gauge: {item.wgust}m/s</h3>
+                        <h3>Weather Condition: {item.conditions}</h3>
+                        <h3>Weather Type: {item.weathertype}</h3>
+                        <h3>Wind direction: {item.wdir} degrees</h3>
+                      </>
+                    );
+                  }}
+                ></a-cylinder>
+              );
+            })}
+          </a-entity>
+          <a-entity ref={windRef}>
+            {data.map((item, index) => {
+              return (
+                <a-cylinder
+                  key={index}
+                  onClick={() => {
+                    toast.success(
+                      <>
+                        <h2>On {item.datetimeStr.slice(0, 10)}</h2>
+                        <h3>Wind Speed: {item.wspd}m/s</h3>
+                        <h3>Weather Condition: {item.conditions}</h3>
+                        <h3>Weather Type: {item.weathertype}</h3>
+                        <h3>Sea Level Pressure: {item.sealevelpressure} hPa</h3>
+                      </>
+                    );
+                  }}
+                ></a-cylinder>
+              );
+            })}
+          </a-entity>
+          <a-plane
+            color="gray"
+            width={width}
+            height={height}
+            rotation="-90 0 0"
+            position="0.5 0 0.8"
+          ></a-plane>
+          <a-entity
+            line="start: -7.2 -0.7 -6; end: -7.2 10 -6"
+            line__2="start: 7.4 -0.7 -6; end: 7.4 10 -6"
+            line__3="start: 7.4 -0.7 5; end: 7.4 10 5"
+          ></a-entity>
+        </a-marker>
+        <a-entity camera></a-entity>
       </a-scene>
     </>
   );
