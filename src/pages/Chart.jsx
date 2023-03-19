b496039b068cc474c5d139050e7dadc8ea7ad6cb
omotosho47 <kmm22njy@bangor.ac.uk>
2023-03-19 07:59:50 +0100

    rotate

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -179,7 +179,7 @@ function Chart() {
       {/* <a-scene ref={sceneRef} embedded arjs> */}
         {/* <a-entity camera></a-entity> */}
         <a-marker preset="hiro">
-        <a-entity position="7 -9 20" rotation="0 45 0">
+        <a-entity position="7 -9 20" rotation="0 0 45">
         <a-box position='0 0.5 0' scale="1 1 1" material='color: yellow;'></a-box>
           <Legend />
           <Text
